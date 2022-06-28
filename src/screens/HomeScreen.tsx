98eb968f384af304fcb421a51c588b905bdf4006
Adriel Baez <baez.adriel27@gmail.com>
2022-06-28 10:25:52 -0300

    first commit

---
+++ src/screens/HomeScreen.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
+import { COLORS, SIZES } from '../constants';
+export const HomeScreen = () => {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <Text style={{ color: COLORS.lightGreen, fontSize: SIZES.h1 }}>
+        HomeScreen!!
+      </Text>
+      <Icon name="star-outline" size={30} color={COLORS.red} />
+    </View>
+  );
+};
